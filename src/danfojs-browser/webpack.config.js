9fe21d8e41c46abf654c0105c77d5fba2caa53f3
Matthew Walowski <mattwalowski@gmail.com>
2024-04-03 09:00:24 -0500

    organize plugins

--- src/danfojs-browser/webpack.config.js
+++ src/danfojs-browser/webpack.config.js
@@ -59,73 +59,80 @@ const createConfig = () => {
         },
         {
           test: /\.m?js$/,
-	  exclude: /core-js/,
+	        exclude: /core-js/,
           use: {
             loader: 'babel-loader',
             options: {
               //presets: ["es2015"],
               plugins: [
-		 //"@babel/plugin-proposal-dynamic-import",
-    		"@babel/plugin-transform-modules-commonjs",
-    		"@babel/plugin-transform-class-properties",
-    		"@babel/plugin-transform-class-static-block",
-    		"@babel/plugin-transform-private-methods",
-    		"@babel/plugin-transform-private-property-in-object",
-    		"@babel/plugin-syntax-top-level-await",
+                // CommonJS
+    		        "@babel/plugin-transform-modules-commonjs",
 
-    		"@babel/plugin-transform-logical-assignment-operators",
-    		"@babel/plugin-transform-numeric-separator",
-
-    		"@babel/plugin-transform-dynamic-import",
-    		"@babel/plugin-transform-export-namespace-from",
-    		"@babel/plugin-transform-nullish-coalescing-operator",
-    		"@babel/plugin-transform-optional-chaining",
-    		"@babel/plugin-syntax-bigint",
-    		"@babel/plugin-syntax-dynamic-import",
-    		"@babel/plugin-syntax-import-meta",
-
-    		"@babel/plugin-transform-optional-catch-binding",
-    		"@babel/plugin-transform-json-strings",
-
-    		"@babel/plugin-transform-async-generator-functions",
-    		"@babel/plugin-transform-object-rest-spread",
-
-    		// "@babel/plugin-transform-async-to-generator",
-    		"@babel/plugin-transform-exponentiation-operator",
-
-    		"@babel/plugin-transform-classes",
-                "@babel/plugin-transform-arrow-functions",
-    		"@babel/plugin-transform-block-scoping",
-    		"@babel/plugin-transform-computed-properties",
-    		"@babel/plugin-transform-destructuring",
-    		"@babel/plugin-transform-duplicate-keys",
-    		"@babel/plugin-transform-for-of",
-    		"@babel/plugin-transform-function-name",
-    		"@babel/plugin-transform-instanceof",
-    		"@babel/plugin-transform-literals",
-    		"@babel/plugin-transform-new-target",
-    		"@babel/plugin-transform-object-super",
-    		"@babel/plugin-transform-parameters",
-    		"@babel/plugin-transform-shorthand-properties",
-    		"@babel/plugin-transform-spread",
-    		"@babel/plugin-transform-template-literals",
-    		"@babel/plugin-transform-typeof-symbol",
-    		"@babel/plugin-transform-unicode-escapes",
-    		"@babel/plugin-transform-property-mutators",
+                // ES 2024
                 "@babel/plugin-transform-unicode-sets-regex",
-                ["@babel/plugin-transform-unicode-property-regex",
-			{
-				useUnicodeFlag: false
-			}
-		],
+
+                // ES 2022
+    		        "@babel/plugin-transform-class-properties",
+    		        "@babel/plugin-transform-class-static-block",
+    		        "@babel/plugin-transform-private-methods",
+                "@babel/plugin-transform-private-property-in-object",
+                "@babel/plugin-syntax-top-level-await",
+
+                // ES 2021
+                "@babel/plugin-transform-logical-assignment-operators",
+                "@babel/plugin-transform-numeric-separator",
+
+                // ES 2020
+                "@babel/plugin-transform-dynamic-import",
+                "@babel/plugin-transform-export-namespace-from",
+                "@babel/plugin-transform-nullish-coalescing-operator",
+                "@babel/plugin-transform-optional-chaining",
+                "@babel/plugin-syntax-bigint",
+                "@babel/plugin-syntax-dynamic-import",
+                "@babel/plugin-syntax-import-meta",
+
+                // ES 2019
+                "@babel/plugin-transform-optional-catch-binding",
+                "@babel/plugin-transform-json-strings",
+
+                // ES 2018
+                "@babel/plugin-transform-async-generator-functions",
+                "@babel/plugin-transform-object-rest-spread",
+                ["@babel/plugin-transform-unicode-property-regex", { useUnicodeFlag: false }],
                 "@babel/plugin-transform-dotall-regex",
-                ["@babel/plugin-transform-named-capturing-groups-regex",
-			{
-				runtime: false
-			}
-		],
+                ["@babel/plugin-transform-named-capturing-groups-regex", { runtime: false }],
+
+                // ES 2017
+    		        "@babel/plugin-transform-async-to-generator",
+
+                // ES 2016
+    		        "@babel/plugin-transform-exponentiation-operator",
+
+                // ES 2015
+                "@babel/plugin-transform-arrow-functions",
+                "@babel/plugin-transform-block-scoping",
+                "@babel/plugin-transform-classes",
+                "@babel/plugin-transform-computed-properties",
+                "@babel/plugin-transform-destructuring",
+                "@babel/plugin-transform-duplicate-keys",
+                "@babel/plugin-transform-for-of",
+                "@babel/plugin-transform-function-name",
+                "@babel/plugin-transform-instanceof",
+                "@babel/plugin-transform-literals",
+                "@babel/plugin-transform-new-target",
+                "@babel/plugin-transform-object-super",
+                "@babel/plugin-transform-parameters",
+                "@babel/plugin-transform-shorthand-properties",
+                "@babel/plugin-transform-spread",
                 "@babel/plugin-transform-sticky-regex",
-                "@babel/plugin-transform-unicode-regex"
+                "@babel/plugin-transform-template-literals",
+                "@babel/plugin-transform-typeof-symbol",
+                "@babel/plugin-transform-unicode-escapes",
+                "@babel/plugin-transform-unicode-regex",
+                "@babel/plugin-transform-property-mutators",
+
+                // Other
+		            "@babel/plugin-transform-regenerator",
               ]
             }
           }
